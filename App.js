6f9d30fb41d3af23c9b502bf9f4b06ffd6737137
rian-kh <128095876+rkhandk@users.noreply.github.com>
2023-11-24 23:47:53 -0500

    Added countdown circle

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TimerPage from './TimerPage.js';
 
+// Dropdown from 0 min to 30 min or something
+
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
--- TimerPage.js
+++ TimerPage.js
@@ -2,15 +2,38 @@ import * as React from 'react';
 import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
+
 
 
 function TimerPage() {
+
+    const [isPlaying, setIsPlaying] = React.useState(true)
+    
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Timer Screen</Text>
-      </View>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+            <CountdownCircleTimer
+                isPlaying={isPlaying}
+                duration={10}
+                colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+                colorsTime={[10, 6, 3, 0]}
+                onComplete={() => ({ shouldRepeat: true, delay: 2 })}
+                updateInterval={1}
+            >
+                {({ remainingTime, color }) => (
+                    <Text style={{ color, fontSize: 40 }}>
+                        {remainingTime}
+                    </Text>
+                )}
+            </CountdownCircleTimer>
+            <Text>Beans milo cat</Text>
+
+            <Button title="Toggle Playing" onPress={() => setIsPlaying(prev => !prev)} />
+
+        </View>
     );
-  }
+}
 
 
 export default TimerPage;
