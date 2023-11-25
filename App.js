55425ac8d3cabe90106b22fdd6fe4924b017e331
Isa <104914002+Isa1d@users.noreply.github.com>
2023-11-25 15:58:08 -0500

    foam

--- App.js
+++ App.js
@@ -1,63 +1,182 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import TimerPage from './TimerPage.js';
-import DropDownPicker from 'react-native-dropdown-picker';
+import { StyleSheet ,Button, View, Text, Image, TouchableOpacity,} from 'react-native';
 
-// Dropdown from 0 min to 30 min or something
 
-function HomeScreen({ navigation }) {
+// Dropdown: Maybe like 1min to 30min
+// At the top, have "Enter custom time..." that lets you use a keypad to type in 
+
+// Maybe add a pause button for the break?
+
+const Header = () => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-
-      <Button
-        title="Timer"
-        onPress={() => navigation.navigate('Timer')}
-      />
-
-      <Button
-        title="Journal"
-        onPress={() => navigation.navigate('Journal')}
-      />
-
-      <Button
-        title="Statistics"
-        onPress={() => navigation.navigate('Statistics')}
-      />
+    <View style={styles.header_container}>
+        <View style={styles.iconContainer}>
+          <Image source={require('./assets/img/menu.png')} style={styles.icon} />
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.header_container_text}>Burnout</Text>
+        </View>
+        <View style={styles.iconContainer}>
+          <View style={styles.icon} />
+        </View>
     </View>
   );
-}
+};
 
-function journalScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Journal Screen</Text>
-    </View>
+const Body = () =>{
+  return(
+        <View style = {styles.body_container}>
+        </View>
   );
-}
+};
 
-function statisticsScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Statistics Screen</Text>
-    </View>
+const Menu = () =>{
+  return(
+        <View style = {styles.menu_container}>
+          
+          <View style = {styles.menu_rectangle}>
+            
+            <TouchableOpacity onPress={() => {/* do this */}}>
+              <View style = {styles.homebtn}>
+                <Image source={require('./assets/img/home.png')}  />
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => {/* do this */}}>
+              <View style = {styles.actionbtn}>
+                <Text style={styles.action_text}>Spark</Text>
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => {/* do this */}}>
+              <View style = {styles.homebtn}>
+                <Image source={require('./assets/img/match.png')}  />
+              </View>
+            </TouchableOpacity>
+
+          </View>
+          
+        </View>
   );
-}
+};
 
-const Stack = createNativeStackNavigator();
+const App = () =>{
+    return(
+      <View style={styles.container}>
+        <Header />
+        <Body />
+        <Menu/>
+     </View>
+    );
+};
 
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Timer" component={TimerPage} />
-        <Stack.Screen name="Journal" component={journalScreen} />
-        <Stack.Screen name="Statistics" component={statisticsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, // Take up the entire height of the screen
+    flexDirection: 'column',
+  },  
+
+  header_container_text: {
+    color: '#6F5044',
+    fontSize: 32, 
+    textDecorationLine: 'underline', 
+    fontWeight: 'bold', 
+    fontFamily: 'SF Pro', 
+  },
+
+  header_container: {
+    flex: 2,
+    backgroundColor: '#EED1B6',
+    flexDirection: 'row',
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 5 }, // Adjust shadow offset
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+    elevation: 5,
+    overflow: 'visible', 
+  },
+
+  body_container: {
+    flex: 12,
+    backgroundColor: '#FFEDDC',
+    flexDirection: 'column',
+    padding: 5,
+  },
+
+  menu_container: {
+    backgroundColor: '#FFEDDC',
+    flex: 2,
+    alignItems: 'center', // Center the text horizontally
+    
+  },
+
+  homebtn: {   
+    backgroundColor: '#EED1B6',
+    width: 57,
+    height: 57,
+    borderRadius: 50,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 5 }, // Adjust shadow offset
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+    elevation: 5,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+
+  action_text:{
+    color: '#6F5044',
+    fontSize: 25, 
+    fontWeight: 'bold', 
+    fontFamily: 'SF Pro',
+  },
+
+  actionbtn: {   
+    backgroundColor: '#EED1B6',
+    width: 131,
+    height: 57,
+    borderRadius: 50,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 5 }, // Adjust shadow offset
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+    elevation: 5,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+
+  menu_rectangle: {
+    backgroundColor: '#EED1B6',
+    width: 337,
+    height: 84,
+    borderRadius: 50,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 5 }, // Adjust shadow offset
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+    elevation: 5,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  
+  icon: {
+    width: 24, // Set the width of your icon
+    height: 24, // Set the height of your icon
+    resizeMode: 'contain', // Adjust the resizeMode as needed
+  },
+
+  iconContainer: {
+    marginRight: 10, // Adjust the margin as needed
+  },
+
+  textContainer: {
+    flex: 1,
+    alignItems: 'center', // Center the text horizontally
+  },
+});
 
 export default App;
